43973452a9375100062767dd0cc4e84a2bdb588f
Agwarota Collins <kollinzomo@gmail.com>
2022-09-11 07:07:38 -0700

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
+#include<stdlib.h>
 /**
-* main - Entry point,Prints out the last part of a quote in the standard error.
-* Return: Always 1 if Success
-*/
+ * main - Entry point
+ *
+ * Return
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return(1);
+}
