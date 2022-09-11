c254d244017d4d423809119fcc8c5ab81c4e9305
Agwarota Collins <kollinzomo@gmail.com>
2022-09-11 12:01:19 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,6 @@
  *
  * Return
  */
-int main(void) 
-{ 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+int main(void) { write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return(1);
 }
