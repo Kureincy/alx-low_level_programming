63211137eae214d3d2cc7955a0f5c45c194babb0
Agwarota Collins <kollinzomo@gmail.com>
2022-09-13 16:35:12 -0700

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,18 +10,16 @@
 
 int _isalpha(int c)
 {
-	char lower, upper;
-	int isletter = 0;
-
-	for (lower = 'a'; lower <= 'z'; lower++)
+	if (c >= 'a' && c <= 'z')
 	{
-		for (upper = 'A'; upper <= 'Z'; upper++)
-		{
-			if (c == lower || c == upper)
-				isletter = 1;
-			else
-				isletter = 0;
-		}
+		return (1);
 	}
-	return (isletter);
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
