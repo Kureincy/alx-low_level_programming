66d34b321e01ac5d18651d24dd03f684c937193b
Agwarota Collins <kollinzomo@gmail.com>
2022-09-13 01:05:25 -0700

    updae

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	putchar('\n');
+
+	return (0);
+}
