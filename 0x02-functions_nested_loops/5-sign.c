8564887817f35a7c387228d219405bc1df7555bf
Agwarota Collins <kollinzomo@gmail.com>
2022-09-13 16:53:40 -0700

    m

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /*
- * print_sign - Entry point
- * @n: carrier variable
- * Description: This function prints the sign of a number
- * Return: 1 if the number is positive, o if the number is 0, and 
- * -1 if the number is negative
- */
+* print_sign: Entry point
+* @n: carrier variable
+* Description: This function prints the sign of a number
+* Return: 1 if the number is positive, o if the number is 0, and 
+* -1 if the number is negative
+*/
 
 int print_sign(int n)
 {
