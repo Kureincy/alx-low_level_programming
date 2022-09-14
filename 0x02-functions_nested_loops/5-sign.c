d717fcbcb033f20271cacc2ce29df8f4d87ab89a
Agwarota Collins <kollinzomo@gmail.com>
2022-09-14 14:37:40 -0700

    m

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_sign: chek main
+* print_sign - check main
 * @n: carrier variable
 * Description: prints the sign of a number
 * Return: 1 if n>0, 0 if n == 0, -1 if n<0
