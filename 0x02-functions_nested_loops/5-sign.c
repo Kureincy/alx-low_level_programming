292ebd34a5ce36b38fa09650fe9cc13ccbcebbcf
Agwarota Collins <kollinzomo@gmail.com>
2022-09-14 14:21:17 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
 * print_sign: Entry point check main
 * @n: carrier variable
 * Description: This function prints the sign of a number
@@ -9,16 +9,23 @@
 
 int print_sign(int n)
 {
+	int value;
+
 	if (n > 0)
 	{
+		value = 1;
 		_putchar('+');
-		return (1);
 	}
-	else if (n < 0)
+	else (if n == 0)
 	{
-		_putchar('-');
-		return (-1);
+		value = 0;
+		_putchar('0');
 	}
 	else
-		return (0);
+	{
+		value = -1;
+		_putchar('-');
+	}
+
+	return (value);
 }
