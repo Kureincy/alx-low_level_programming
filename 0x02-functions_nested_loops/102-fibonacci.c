56b9fe29349535bd40963ac7f761a76ae07b2ef5
Agwarota Collins <kollinzomo@gmail.com>
2022-09-14 17:36:27 -0700

    m

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,23 +8,20 @@
 int main(void)
 {
 	int i;
-	long j = 1, k = 2;
+	long int fibonacci[50];
 
-	while (i < 50)
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
+
+	for (i = 2; i < 50; i++)
 	{
-		if (i == 0)
-			printf("%ld", j);
-		else if (i == 1)
-			printf(", %ld", k);
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		if (i == 49)
+			printf("%ld\n", fibonacci[i]);
 		else
-		{
-			k += j;
-			j = k - j;
-			printf(", %ld", k);
-		}
-
-		++i;
+			printf("%ld, ", fibonacci[i]);
 	}
-	printf("\n");
+
 	return (0);
 }
